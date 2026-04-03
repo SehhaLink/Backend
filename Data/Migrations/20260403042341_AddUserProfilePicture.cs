bfbd2e4e6e64fc4e2eb10379b107922d6bf3fc36
Abdulrahman <abdoawwad043@gmail.com>
2026-04-03 06:28:39 +0200

    add user image migrations

---
+++ Data/Migrations/20260403042341_AddUserProfilePicture.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sehha360.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserProfilePicture : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePictureKey",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePictureKey",
+                table: "AspNetUsers");
+        }
+    }
+}
